ec519260c773fee0e2af5df9aad50d5fa337c51a
seleniumhina <hinainamdar678@gmail.com>
2024-07-27 14:45:32 +0530

    test injelji

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+        System.out.println("Welcome to Java!");
 
 
         }
